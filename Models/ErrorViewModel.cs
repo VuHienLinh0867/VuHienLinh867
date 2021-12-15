f332a7aa7d50c94186e37aef8b87d70da31c364d
Vu Hien Linh <hienlinh0211@gmail.com>
2021-12-15 13:08:33 +0700

    First_commit_VHL867

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VuHienLinh867.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
